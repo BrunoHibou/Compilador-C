bc91fdc9bd898e09e80a96e2a58b77bf29667c7c
BrunoHibou <brunocavalcanticosta.bc@gmail.com>
2024-04-10 08:41:43 +0000

    dia D

--- new/main.py
+++ new/main.py
@@ -3,16 +3,7 @@ from dict import *
 
 
 def main():
-    nome =  "/workspaces/Compilador-C/new/teste1.c"
-    programa = ler_arquivo(nome)
-    original = open(nome, 'r')
-    print(original.read())
-
-
-    rules = [find_text_constants, Find_reserved, find_ops, find_numbers, find_delimiters, find_identifiers]
-    for rule in rules:
-        programa = rule(programa)
-        print('\n-----------------\n' + programa + '\n-----------------\n')
+    analyser = Utils()
 
 if __name__ == "__main__":
     print("----------------- Programa Iniciado -----------------")
